25322fd6ec78f7d5fc18e7f7de15b912e59ee86f
Mike <162806810+MikeMalyshev@users.noreply.github.com>
2024-06-30 09:24:06 +0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Yandex-Practicum/go-rest-api-homework
+
+go 1.20
+
+require github.com/go-chi/chi/v5 v5.0.10 // indirect
---
+++ precode.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+// Task ...
+type Task struct {
+	ID           string   `json:"id"`
+	Description  string   `json:"description"`
+	Note         string   `json:"note"`
+	Applications []string `json:"applications"`
+}
+
+var tasks = map[string]Task{
+	"1": {
+		ID:          "1",
+		Description: "Сделать финальное задание темы REST API",
+		Note:        "Если сегодня сделаю, то завтра будет свободный день. Ура!",
+		Applications: []string{
+			"VS Code",
+			"Terminal",
+			"git",
+		},
+	},
+	"2": {
+		ID:          "2",
+		Description: "Протестировать финальное задание с помощью Postmen",
+		Note:        "Лучше это делать в процессе разработки, каждый раз, когда запускаешь сервер и проверяешь хендлер",
+		Applications: []string{
+			"VS Code",
+			"Terminal",
+			"git",
+			"Postman",
+		},
+	},
+}
+
+// Ниже напишите обработчики для каждого эндпоинта
+// ...
+
+func main() {
+	r := chi.NewRouter()
+
+	// здесь регистрируйте ваши обработчики
+	// ...
+
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
+		return
+	}
+}
